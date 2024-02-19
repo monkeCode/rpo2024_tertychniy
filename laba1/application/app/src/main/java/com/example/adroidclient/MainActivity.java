48ed3720014f17b4bdd435c2ef09f145125461f3
Georgy <georgy.tertychniy@yandex.ru>
2024-02-20 02:33:57 +0300

    completed

--- laba1/application/app/src/main/java/com/example/adroidclient/MainActivity.java
+++ laba1/application/app/src/main/java/com/example/adroidclient/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.adroidclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.widget.TextView;
 
 import com.example.adroidclient.databinding.ActivityMainBinding;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'adroidclient' library on application startup.
     static {
         System.loadLibrary("adroidclient");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
@@ -24,8 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
+        int num = initRng();
+        byte[] key = randomBytes(10);
+
+        String s = "super test text";
+        byte[] testData = s.getBytes(StandardCharsets.UTF_8);
+
+        byte[] encrypted = encrypt(key, testData);
+        byte[] decrypted = encrypt(key, encrypted);
+
+        String res = new String(decrypted, StandardCharsets.UTF_8);
         TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        tv.setText(s);
     }
 
     /**
@@ -33,4 +49,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key,byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+
 }
